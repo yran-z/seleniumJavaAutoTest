0d0454174fe86135658c5af28eaf33e038ef958a
yran-z <1647051937@qq.com>
2020-09-22 01:41:24 +0800

    feat:新建项目

---
+++ src/main/java/AutoTest.java
@@ -0,0 +1,5 @@
+public class AutoTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C://Program Files (x86)//Google//Chrome//Application//chromedriver_win32//chromedriver.exe");
+    }
+}
